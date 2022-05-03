61b031796031c3943d93309a7cae51b80600f362
Guilherme Soares <guilherme.limasoares@hotmail.com>
2022-05-03 11:05:25 -0300

    bloco-6

---
+++ Fundamentos do desenvolvimento web/Bloco-4- Introducao-a-JavaScript-e-logica-de-Programacao/exercicios/4.1.js
@@ -0,0 +1,19 @@
+
+let classificacao = "Guilherme"
+
+switch (classificacao) {
+    case "Aprovada":
+        console.log("Parabéns, você foi aprovada =)");
+        break;
+    
+    case "reprovada":
+        console.log("Que pena, você foi reprovada =(")
+        break;
+    
+    case "lista":
+        console.log("Seu nome segue na lista");
+        break;
+
+    default:
+        console.log("Não se aplica")
+}
